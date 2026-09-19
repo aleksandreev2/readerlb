31c695a3c8f1321e5d009ee872b1d7b2bd8b564b
aleksandreev2 <302271325+aleksandreev2@users.noreply.github.com>
2026-09-19 22:18:40 +0400

    test: compare preserved chapter numbers as raw strings

--- app/src/test/java/com/readerlb/app/importer/RanobeLibPackageBuilderTest.kt
+++ app/src/test/java/com/readerlb/app/importer/RanobeLibPackageBuilderTest.kt
@@ -44,7 +44,6 @@ class RanobeLibPackageBuilderTest {
                 chapters
                     .getJSONObject(it)
                     .getString("number")
-                    .toInt()
             }
         )
 
