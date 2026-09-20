a7ef4dcb2ba68cc6578b19c4504339055e0426e3
aleksandreev2 <sashahumortele3@gmail.com>
2026-09-20 23:38:55 +0400

    release: bump ReaderLB to 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.readerlb.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.5.0"
+        versionCode = 9
+        versionName = "0.9.0"
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
     }
