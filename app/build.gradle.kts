3411b86d254917a15a31b44284f91fc3637cdf65
aleksandreev2 <sashahumortele3@gmail.com>
2026-09-20 20:15:41 +0400

    chore: bump ReaderLB to 0.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.readerlb.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.4.1"
+        versionCode = 7
+        versionName = "0.4.2"
     }
 
     compileOptions {
