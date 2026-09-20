a7f94165398786f3bd3e7b6284e4945522e3af11
aleksandreev2 <sashahumortele3@gmail.com>
2026-09-20 23:25:15 +0400

    ui: adopt RanobeLib-inspired dark palette

--- app/src/main/java/com/readerlb/app/ui/theme/Color.kt
+++ app/src/main/java/com/readerlb/app/ui/theme/Color.kt
@@ -2,13 +2,13 @@ package com.readerlb.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Navy = Color(0xFF061525)
-val Navy2 = Color(0xFF0A2038)
-val Blue = Color(0xFF1687EF)
+val Navy = Color(0xFF071018)
+val Navy2 = Color(0xFF101A24)
+val Blue = Color(0xFF2D9CFF)
 val Cyan = Color(0xFF26CCFF)
-val Ink = Color(0xFF101521)
-val Muted = Color(0xFF667085)
-val Canvas = Color(0xFFF6F8FB)
-val Card = Color(0xFFFFFFFF)
-val Line = Color(0xFFE4EAF2)
-val Success = Color(0xFF12B886)
+val Ink = Color(0xFFF2F4F8)
+val Muted = Color(0xFFA8B0BE)
+val Canvas = Color(0xFF0F1115)
+val Card = Color(0xFF171A20)
+val Line = Color(0xFF2A2F38)
+val Success = Color(0xFF5DD6AA)
