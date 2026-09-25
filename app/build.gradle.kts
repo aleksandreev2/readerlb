4f31ae2d04c24c531c8ade87e1163f04ddddf7aa
aleksandreev2 <302271325+aleksandreev2@users.noreply.github.com>
2026-09-26 01:39:37 +0400

    Prepare 1.1.1 HTML export hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 19
-        versionName = "1.1.0"
+        versionName = "1.1.1"
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
     }
