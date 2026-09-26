58b229d57cb1adcb9b4e09c50110da6a9f58e8b1
aleksandreev2 <302271325+aleksandreev2@users.noreply.github.com>
2026-09-26 11:06:32 +0400

    Bump 1.1.1 candidate versionCode to 20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.readerlb.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 19
+        versionCode = 20
         versionName = "1.1.1"
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
